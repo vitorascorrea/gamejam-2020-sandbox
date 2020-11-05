967e2abd473c72c6daeba2cd5803635b9732cb5a
Vitor <vitorascorrea@gmail.com>
2020-11-05 13:31:49 -0300

    Add GridWorld scene
    - Walk in four directions
    - Run with shift pressed

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Phaser from 'phaser'
 
 import HelloWorldScene from './scenes/HelloWorldScene'
+import GridWorldScene from './scenes/GridWorldScene'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
@@ -9,10 +10,10 @@ const config: Phaser.Types.Core.GameConfig = {
 	physics: {
 		default: 'arcade',
 		arcade: {
-			gravity: { y: 200 }
+			// gravity: { y: 200 }
 		}
 	},
-	scene: [HelloWorldScene]
+	scene: [GridWorldScene]
 }
 
 export default new Phaser.Game(config)
--- src/scenes/HelloWorldScene.ts
+++ src/scenes/HelloWorldScene.ts
@@ -9,7 +9,7 @@ export default class HelloWorldScene extends Phaser.Scene {
   scoreText: any;
   bombs: any;
   jumpCount: number = 0;
-  canJump: boolean;
+  canJump: boolean = true;
 
   constructor() {
     super('gamejam2020');
